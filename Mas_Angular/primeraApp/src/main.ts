cdbdb91601f16cfdc8d21e073e574cc4b9c08fb6
Dav <dabghos@outlook.com>
2023-04-18 22:59:40 +0200

    Cosas

--- Mas_Angular/primeraApp/src/main.ts
+++ Mas_Angular/primeraApp/src/main.ts
@@ -3,5 +3,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule) //define el módulo raíz, que es el módulo encargado de cargar la aplicación
   .catch(err => console.error(err));
