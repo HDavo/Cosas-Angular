84f6bc78b9d33e6e0036f1307812cce7602d0ecd
HDavo <dabghos@outlook.com>
2023-04-20 11:23:48 +0200

    Final de mapas

--- Angular/09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ Angular/09-mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,13 +1,160 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface MarcadorColor{
+  color: string;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number];
+}
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+   `
+     .mapa-container{
+      width: 100%;
+      height: 100%;
+    }
+
+    .list-group{
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+
+    li{
+      cursor: pointer;
+    }
+   `
   ]
 })
-export class MarcadoresComponent{
- 
+export class MarcadoresComponent implements AfterViewInit{
+  
+  @ViewChild('mapa') divMapa!: ElementRef; //TODO: buscar información de viewchild y completar las notas con su funcionamiento
+  mapa!: mapboxgl.Map;  //esta variable tiene este tipado
+  zoomLevel: number = 15;
+  inicio: [number, number] = [ -3.7503986001651857, 40.39743386973446 ];
+
+  //array de marcadores
+  marcadores: MarcadorColor[] = [];
+
+  constructor(){}
+
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, //se debe poner asi por ser una referencia al elemento HTML
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.inicio,
+      zoom: this.zoomLevel
+     });
+
+
+     //se pone aqui para que si existen marcadores, se pongan una vez se ha generado el mapa
+     this.leerLocalStorage();
+
+     //una manera de poner un marcador personalizado
+     /* const marcadorHtml: HTMLElement = document.createElement('div'); 
+     marcadorHtml.innerHTML = 'Hola'; */
+
+     //marcador NO DINÁMICO
+    /*  const marcador = new mapboxgl.Marker({
+      element: marcadorHtml
+     }) //marcador dentro del mapa en la posición que hemos elegido por defecto
+      .setLngLat(this.inicio)
+      .addTo(this.mapa) */
+  }
+
+
+  agregarMarcador(){
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    console.log(color);
+
+
+    const nuevoMarcador = new mapboxgl.Marker({
+      draggable: true,
+      color
+    })
+      .setLngLat( this.inicio)
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+
+    // console.log(this.marcadores);
+    this.guardarMarcadorLocalStorage(); //se llama aqui al método porque asi guardamos un marcador dentro del localstorage cada vez que se guarda
+
+    nuevoMarcador.on('dragend',() => {
+      this.guardarMarcadorLocalStorage();
+    });
+  }
+
+  irMarcador(marker: mapboxgl.Marker){
+    // console.log(marker);
+
+    this.mapa.flyTo({
+      center: marker.getLngLat(),
+    });
+  }
+
+
+  guardarMarcadorLocalStorage(){
+
+    const lngLatArr: MarcadorColor[] = [];
+
+    this.marcadores.forEach( m => {
+      const color = m.color;
+      const {lng, lat} = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: m.color,
+        centro: [lng, lat],
+      });
+    })
+
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr)); //se usa el stringify porque dentro del localstorage solo podemos tener strings
+  }
+
+  leerLocalStorage(){
+
+    if(!localStorage.getItem('marcadores')){ //comprobamos que no existe dentro del localStorage
+      return;
+    }
+
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+
+    // console.log(lngLatArr);
+    lngLatArr.forEach( m =>  {
+      
+      const nuevoMarcador = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true
+      })
+      .setLngLat( m.centro!)
+      .addTo( this.mapa);
+
+      this.marcadores.push({
+        marker: nuevoMarcador,
+        color: m.color
+      });
+
+      nuevoMarcador.on('dragend',() => {
+        this.guardarMarcadorLocalStorage();
+      });
+
+    });
+  }
+
+  borrarMarcador(i: number){
+    console.log('Borrando marcador');
+    this.marcadores[i].marker?.remove(); //esto lo borra del mapa
+    this.marcadores.splice(i, 1); //lo borra del elemento html que lo muestra
+    this.guardarMarcadorLocalStorage(); //actualizamos los marcadores guardados después de borrar
+
+  }
+
   
 }
--- Angular/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ Angular/09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -18,17 +18,77 @@ import * as mapboxgl from 'mapbox-gl';
         padding: 10px;
         position: fixed;
         z-index: 999;
+        width: 400px;
       }
     `
   ]
 })
-export class ZoomRangeComponent implements OnInit{
-  ngOnInit(): void {
-    var map = new mapboxgl.Map({
-     container: 'mapa',
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy{
+
+  @ViewChild('mapa') divMapa!: ElementRef; //TODO: buscar información de viewchild y completar las notas con su funcionamiento
+  mapa!: mapboxgl.Map;  //esta variable tiene este tipado
+  zoomLevel: number = 10;
+  inicio: [number, number] = [ -3.7503986001651857, 40.39743386973446 ];
+
+  constructor(){}
+
+  ngOnDestroy(): void { //debe ponerse un off por cada de uno de los listeners que tengamos definidos
+    this.mapa.off('zoom',() => {});
+    this.mapa.off('zoomend',() => {});
+    this.mapa.off('move',() => {});
+  }
+
+  ngAfterViewInit(): void {
+
+    // console.log('afterviewinit', this.divMapa);
+    this.mapa = new mapboxgl.Map({
+     container: this.divMapa.nativeElement, //se debe poner asi por ser una referencia al elemento HTML
      style: 'mapbox://styles/mapbox/streets-v11',
-     center: [ -3.7503986001651857, 40.39743386973446 ],
-     zoom: 12
+     center: this.inicio,
+     zoom: this.zoomLevel
     });
+
+    //para saber el nivel del zoom de manera precisa
+    this.mapa.on('zoom', (ev) => {
+  
+      /* Podriamos sacar el nivel de zoom de diferente maneras, una de ellas:
+      const zoomActual = this.mapa.getZoom(); //este es el nivel de zoom actual cuando movemos */
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', (ev) => { //definimos un nivel de zoom máximo
+
+      if(this.mapa.getZoom() > 18){
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    //para poder indicar las coordenadas a medida que nos movemos dentro del mapa
+    this.mapa.on('move', (event) => {
+
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.inicio = [lng, lat];
+
+      // console.log(target.getCenter());
+    })
+ }
+
+
+ zoomOut(){
+  this.mapa.zoomOut();
+
+
+ }
+
+ zoomIn(){
+  this.mapa.zoomIn();
+
+  
+ }
+
+ cambioZoom(valor: string){
+  console.log(valor);
+  this.mapa.zoomTo(Number(valor));
  }
 }
