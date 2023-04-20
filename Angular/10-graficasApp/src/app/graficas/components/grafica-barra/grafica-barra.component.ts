94e508cb19a65b7f0d69b08aa6da5e7947ccd10a
HDavo <dabghos@outlook.com>
2023-04-20 14:10:38 +0200

    appGraficas: Antes de circulares

--- Angular/10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ Angular/10-graficasApp/src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartType, ChartData, ChartDataset } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -7,5 +9,72 @@ import { Component } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent {
+  // ===========
+  // ### Inputs
+  // ===========
+ 
+  @Input() horizontal: boolean = false;
+  @Input() inputLabels!: any[] | unknown[] | undefined;
+  @Input() inputDatasets!: ChartDataset<any, any>[];
+ 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+ 
+  // ================
+  // ### propiedades
+  // ================
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+ 
+    scales: {
+      x: {},
+      y: {},
+    },
+    plugins: {
+      legend: {
+        display: true, // para tener la leyenda si o no
+      },
+    },
+ 
+    indexAxis: 'x',
+  };
+ 
+  public barChartType: ChartType = 'bar'; // los tipos de gráfica
 
+ 
+  // propiedad e mi gráfica -> barCharData
+  // -------------------------------------
+ 
+  // ALGUNAS PROPIEDADES INTERESANTES:
+  // backgroundColor -> para definir el color de la barra
+  // hoverBackgroundColor -> cuando hago click sobre la barra, me sale un color
+  public barChartData: ChartData<'bar'> = {
+    // ==> REFACTORIZO ESTO UTILIZANDO LOS INPUTS!!
+    labels: [],
+    datasets: [],
+  };
+ 
+  // ================
+  // ### constructor
+  // ================
+  constructor() {
+    //console.log(this.horizontal);
+  }
+ 
+  // ================
+  // ### ngOnInit
+  // ================
+  ngOnInit(): void {
+    //console.log(this.horizontal); // aquí si recibo en un inicio la propiedad cuando se construyen las gráficas
+    if (this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';  
+    }
+ 
+    //console.log(this.inputDatasets); 
+    //console.log(this.inputLabels);
+ 
+    this.barChartData.datasets = this.inputDatasets;
+    this.barChartData.labels = this.inputLabels;
+  }
 }
+
+
--- Angular/10-graficasApp/src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ Angular/10-graficasApp/src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartDataset } from 'chart.js';
 
 @Component({
   selector: 'app-barras-doble',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   ]
 })
 export class BarrasDobleComponent {
+  proveedoresData: ChartData<'bar'> = {
+    labels: ['2011', '2012', '2013', '2014', '2015'],
+    datasets: [
+      {
+        data: [100, 200, 300, 400, 500],
+        label: 'Vendedor A',
+      },
+      {
+        data: [50, 250, 30, 450, 200],
+        label: 'Vendedor B',
+      },
+    ],
+  };
+ 
+  // ### propiedad : productoData
+  productoData: ChartData<'bar'> = {
+    datasets: [
+      {
+        data: [200, 300, 400, 300, 100],
+        label: 'Tablets',
+        backgroundColor: '#BE28E0',
+      },
+    ],
+  };
 
 }
