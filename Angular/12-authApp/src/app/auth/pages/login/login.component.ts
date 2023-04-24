9e519e2bf40d47831e68d7e5b5d928e0e348bc02
HDavo <dabghos@outlook.com>
2023-04-24 12:28:05 +0200

    Antes de conexión

--- Angular/12-authApp/src/app/auth/pages/login/login.component.ts
+++ Angular/12-authApp/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginComponent {
+
+  miFormulario: FormGroup = this.construirFormulario.group({
+    email: ['pepe1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private construirFormulario: FormBuilder,
+              private router: Router){}
+
+
+  login(){
+    console.log(this.miFormulario.valid);
+    console.log(this.miFormulario.value);
+
+    this.router.navigateByUrl('/dashboard')
+  }
 
 }
--- Angular/12-authApp/src/app/auth/pages/register/register.component.ts
+++ Angular/12-authApp/src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterComponent {
+
+  miFormulario: FormGroup = this.construirFormulario.group({
+    name: ['Pepe 3',[Validators.required]],
+    email: ['pepe3@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private construirFormulario: FormBuilder,
+              private router: Router){}
+
+
+  registro(){
+    // console.log(this.miFormulario.valid);
+    console.log(this.miFormulario.value);
+
+    this.router.navigateByUrl('/dashboard')
+  }
 
 }
